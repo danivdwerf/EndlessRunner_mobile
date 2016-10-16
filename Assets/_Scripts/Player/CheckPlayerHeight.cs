c3b23f2a6de52d92b31d4e7ea6caff257dae006e
Dani van der Werf <danivdwerf@danis-mbp.home>
2016-10-16 10:28:30 +0200

    reupload project

---
+++ Assets/_Scripts/Player/CheckPlayerHeight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPlayerHeight : MonoBehaviour 
+{
+    //private Die die;
+    private PlayerMovement playerMovement;
+    private void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        //die = GetComponent<Die>();
+    }
+
+    private void Update()
+    {
+		/*
+        if (transform.position.y <= -1&&!playerMovement.Death)
+        {
+            //die.KillPlayer();
+        }
+*/
+    }
+}
